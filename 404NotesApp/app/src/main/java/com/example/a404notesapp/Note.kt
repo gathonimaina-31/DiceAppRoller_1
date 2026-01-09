9f69399d073bb007399d9614358a6eecdae854ec
gathonimaina-31 <gathoni.maina@strathmore.edu>
2026-01-09 09:41:33 +0300

    Delete 404NotesApp directory

--- 404NotesApp/app/src/main/java/com/example/a404notesapp/Note.kt
+++
@@ -1,8 +0,0 @@
-package com.example.a404notesapp
-
-data class Note(
-    val id: Long = 0,
-    val title: String = "",
-    val content: String = "",
-    val timestamp: Long = System.currentTimeMillis()
-)
